75f7b6dfb4810ad87561e50b5ef1b48e07338e59
gab <gabrielbordeaux@gmail.com>
2023-04-11 13:46:50 -0400

    Add app context

--- src/unittest/test_models.py
+++ src/unittest/test_models.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 from .base import BaseTest
 from .. import models
+from ..bootstrap import get_or_create_app
 
 
 class Test(BaseTest):
@@ -12,12 +13,16 @@ class Test(BaseTest):
     route = None
     callback = None
 
+    def setUp(self):
+        self.app = get_or_create_app()
+
     def tearDown(self):
-        if self.route:
-            models.db.session.delete(self.route)
-        if self.callback:
-            models.db.session.delete(self.callback)
-        models.db.session.commit()
+        with self.app.app_context():
+            if self.route:
+                models.db.session.delete(self.route)
+            if self.callback:
+                models.db.session.delete(self.callback)
+            models.db.session.commit()
 
     def test_db(self):
         self.assertIsInstance(models.db, SQLAlchemy)
@@ -27,50 +32,55 @@ class Test(BaseTest):
         assert models.db_auto_create() is False
 
     def test_db_auto_create_2(self):
-        # Drop test table to force a db re-creation
-        models.RouteModel.__table__.drop(models.db.engine)
-        models.CallbackModel.__table__.drop(models.db.engine)
+        with self.app.app_context():
+            # Drop test table to force a db re-creation
+            models.RouteModel.__table__.drop(models.db.engine)
+            models.CallbackModel.__table__.drop(models.db.engine)
 
-        # The db will be created and the method is expected to return True
-        assert models.db_auto_create() is True
+            # The db will be created and the method is expected to return True
+            assert models.db_auto_create() is True
 
     def test_RouteModel(self):
-        self.route = models.RouteModel(path=str(uuid.uuid4()))
-        models.db.session.add(self.route)
-        models.db.session.commit()
+        with self.app.app_context():
+            self.route = models.RouteModel(path=str(uuid.uuid4()))
+            models.db.session.add(self.route)
+            models.db.session.commit()
 
-        self.assertIsInstance(self.route, models.RouteModel)
-        self.assertIsInstance(self.route.id, int)
-        self.assertIsInstance(self.route.path, str)
-        self.assertIsInstance(self.route.creation_date, datetime)
-        self.assertIsInstance(self.route.expiration_date, datetime)
-        self.assertIsNone(self.route.name)
+            self.assertIsInstance(self.route, models.RouteModel)
+            self.assertIsInstance(self.route.id, int)
+            self.assertIsInstance(self.route.path, str)
+            self.assertIsInstance(self.route.creation_date, datetime)
+            self.assertIsInstance(self.route.expiration_date, datetime)
+            self.assertIsNone(self.route.name)
 
     def test_RouteModel_repr(self):
-        self.route = models.RouteModel(path=str(uuid.uuid4()))
-        models.db.session.add(self.route)
-        models.db.session.commit()
+        with self.app.app_context():
+            self.route = models.RouteModel(path=str(uuid.uuid4()))
+            models.db.session.add(self.route)
+            models.db.session.commit()
 
-        assert '<Route' in self.route.__repr__()
+            assert '<Route' in self.route.__repr__()
 
     def test_CallbackModel(self):
-        self.route = models.RouteModel(path=str(uuid.uuid4()))
-        models.db.session.add(self.route)
-        models.db.session.commit()
+        with self.app.app_context():
+            self.route = models.RouteModel(path=str(uuid.uuid4()))
+            models.db.session.add(self.route)
+            models.db.session.commit()
 
-        self.callback = models.CallbackModel(route_id=self.route.id)
-        models.db.session.add(self.callback)
-        models.db.session.commit()
+            self.callback = models.CallbackModel(route_id=self.route.id)
+            models.db.session.add(self.callback)
+            models.db.session.commit()
 
-        self.assertIsInstance(self.callback, models.CallbackModel)
+            self.assertIsInstance(self.callback, models.CallbackModel)
 
     def test_CallbackModel_repr(self):
-        self.route = models.RouteModel(path=str(uuid.uuid4()))
-        models.db.session.add(self.route)
-        models.db.session.commit()
+        with self.app.app_context():
+            self.route = models.RouteModel(path=str(uuid.uuid4()))
+            models.db.session.add(self.route)
+            models.db.session.commit()
 
-        self.callback = models.CallbackModel(route_id=self.route.id)
-        models.db.session.add(self.callback)
-        models.db.session.commit()
+            self.callback = models.CallbackModel(route_id=self.route.id)
+            models.db.session.add(self.callback)
+            models.db.session.commit()
 
-        assert '<Callback' in self.callback.__repr__()
+            assert '<Callback' in self.callback.__repr__()
--- src/unittest/test_routes_handler.py
+++ src/unittest/test_routes_handler.py
@@ -5,6 +5,7 @@ from dateparser import parse
 from .base import BaseTest
 from .. import routes_handler
 from ..models import db, RouteModel, CallbackModel
+from ..bootstrap import get_or_create_app
 
 
 class Test(BaseTest):
@@ -16,118 +17,126 @@ class Test(BaseTest):
     route_2_callback_1 = None
     route_2_callback_2 = None
 
+    def setUp(self):
+        self.app = get_or_create_app()
+
     def tearDown(self):
-        if self.route_1:
-            db.session.delete(self.route_1)
-        if self.route_2:
-            db.session.delete(self.route_2)
-        if self.route_1_callback_1:
-            db.session.delete(self.route_1_callback_1)
-        if self.route_1_callback_2:
-            db.session.delete(self.route_1_callback_2)
-        if self.route_2_callback_1:
-            db.session.delete(self.route_2_callback_1)
-        if self.route_2_callback_2:
-            db.session.delete(self.route_2_callback_2)
-        db.session.commit()
+        with self.app.app_context():
+            if self.route_1:
+                db.session.delete(self.route_1)
+            if self.route_2:
+                db.session.delete(self.route_2)
+            if self.route_1_callback_1:
+                db.session.delete(self.route_1_callback_1)
+            if self.route_1_callback_2:
+                db.session.delete(self.route_1_callback_2)
+            if self.route_2_callback_1:
+                db.session.delete(self.route_2_callback_1)
+            if self.route_2_callback_2:
+                db.session.delete(self.route_2_callback_2)
+            db.session.commit()
 
     def test_new(self):
-        route = routes_handler.new()
+        with self.app.app_context():
+            route = routes_handler.new()
 
-        self.assertIsInstance(route, RouteModel)
-        self.assertIsInstance(route.id, int)
-        self.assertIsInstance(route.path, str)
-        assert len(route.path) == 36
+            self.assertIsInstance(route, RouteModel)
+            self.assertIsInstance(route.id, int)
+            self.assertIsInstance(route.path, str)
+            assert len(route.path) == 36
 
     def test_rename(self):
-        route = routes_handler.new()
+        with self.app.app_context():
+            route = routes_handler.new()
 
-        # Default name should be None
-        assert route.name is None
+            # Default name should be None
+            assert route.name is None
 
-        # Rename route with empty name
-        routes_handler.rename(route, '')
-        assert route.name is None
+            # Rename route with empty name
+            routes_handler.rename(route, '')
+            assert route.name is None
 
-        # Rename route with a valid name
-        routes_handler.rename(route, 'New route name')
-        assert route.name == 'New route name'
+            # Rename route with a valid name
+            routes_handler.rename(route, 'New route name')
+            assert route.name == 'New route name'
 
     def test_cleanup_old_routes(self):
-        # Create 2 routes, one expired
-        self.route_1 = RouteModel(path=str(uuid.uuid4()), )
-        db.session.add(self.route_1)
-        self.route_2 = RouteModel(path=str(uuid.uuid4()),
-                                  expiration_date=parse('1 month ago'))
-        db.session.add(self.route_2)
+        with self.app.app_context():
+            # Create 2 routes, one expired
+            self.route_1 = RouteModel(path=str(uuid.uuid4()), )
+            db.session.add(self.route_1)
+            self.route_2 = RouteModel(path=str(uuid.uuid4()),
+                                      expiration_date=parse('1 month ago'))
+            db.session.add(self.route_2)
 
-        db.session.commit()
+            db.session.commit()
 
-        # Add some callback rows
-        self.route_1_callback_1 = CallbackModel(route_id=self.route_1.id)
-        db.session.add(self.route_1_callback_1)
-        self.route_1_callback_2 = CallbackModel(route_id=self.route_1.id)
-        db.session.add(self.route_1_callback_2)
-        self.route_2_callback_1 = CallbackModel(route_id=self.route_2.id)
-        db.session.add(self.route_2_callback_1)
-        self.route_2_callback_2 = CallbackModel(route_id=self.route_2.id)
-        db.session.add(self.route_2_callback_2)
+            # Add some callback rows
+            self.route_1_callback_1 = CallbackModel(route_id=self.route_1.id)
+            db.session.add(self.route_1_callback_1)
+            self.route_1_callback_2 = CallbackModel(route_id=self.route_1.id)
+            db.session.add(self.route_1_callback_2)
+            self.route_2_callback_1 = CallbackModel(route_id=self.route_2.id)
+            db.session.add(self.route_2_callback_1)
+            self.route_2_callback_2 = CallbackModel(route_id=self.route_2.id)
+            db.session.add(self.route_2_callback_2)
 
-        db.session.commit()
+            db.session.commit()
 
-        # Call cleanup method
-        routes_handler.cleanup_old_routes()
+            # Call cleanup method
+            routes_handler.cleanup_old_routes()
 
-        # First route and its callback should exist
-        self.assertIsInstance(RouteModel.query.filter_by(
-            path=self.route_1.path).first(), RouteModel)
+            # First route and its callback should exist
+            self.assertIsInstance(RouteModel.query.filter_by(
+                path=self.route_1.path).first(), RouteModel)
 
-        callbacks = CallbackModel.query.filter_by(
-            route_id=self.route_1.id).all()
-        self.assertIsInstance(callbacks, list)
-        assert len(callbacks) == 2
-        for callback in callbacks:
-            self.assertIsInstance(callback, CallbackModel)
+            callbacks = CallbackModel.query.filter_by(
+                route_id=self.route_1.id).all()
+            self.assertIsInstance(callbacks, list)
+            assert len(callbacks) == 2
+            for callback in callbacks:
+                self.assertIsInstance(callback, CallbackModel)
 
-        # Second route and it's callback should be deleted
-        assert RouteModel.query.filter_by(
-            path=self.route_2.path).first() is None
+            # Second route and it's callback should be deleted
+            assert RouteModel.query.filter_by(
+                path=self.route_2.path).first() is None
 
-        callbacks = CallbackModel.query.filter_by(
-            route_id=self.route_2.id).all()
-        self.assertIsInstance(callbacks, list)
-        assert len(callbacks) == 0
+            callbacks = CallbackModel.query.filter_by(
+                route_id=self.route_2.id).all()
+            self.assertIsInstance(callbacks, list)
+            assert len(callbacks) == 0
 
-        # unset deleted routes
-        self.route_2 = None
-        self.route_2_callback_1 = None
-        self.route_2_callback_2 = None
+            # unset deleted routes
+            self.route_2 = None
+            self.route_2_callback_1 = None
+            self.route_2_callback_2 = None
 
     def test_delete(self):
 
-        # Create 2 routes, one expired
-        route = RouteModel(path=str(uuid.uuid4()), )
-        db.session.add(route)
+        with self.app.app_context():
+            # Create 2 routes, one expired
+            route = RouteModel(path=str(uuid.uuid4()), )
+            db.session.add(route)
 
-        db.session.commit()
+            db.session.commit()
 
-        # Add some callback rows
-        callback_1 = CallbackModel(route_id=route.id)
-        db.session.add(callback_1)
-        callback_2 = CallbackModel(route_id=route.id)
-        db.session.add(callback_2)
+            # Add some callback rows
+            callback_1 = CallbackModel(route_id=route.id)
+            db.session.add(callback_1)
+            callback_2 = CallbackModel(route_id=route.id)
+            db.session.add(callback_2)
 
-        db.session.commit()
+            db.session.commit()
 
-        # Call route deletion method
-        routes_handler.delete(route)
+            # Call route deletion method
+            routes_handler.delete(route)
 
-        # Verify that the route is deleted
-        assert RouteModel.query.filter_by(
-            path=route.path).first() is None
+            # Verify that the route is deleted
+            assert RouteModel.query.filter_by(
+                path=route.path).first() is None
 
-        # Verify that the callbacks are deleted
-        callbacks = CallbackModel.query.filter_by(
-            route_id=route.id).all()
-        self.assertIsInstance(callbacks, list)
-        assert len(callbacks) == 0
+            # Verify that the callbacks are deleted
+            callbacks = CallbackModel.query.filter_by(
+                route_id=route.id).all()
+            self.assertIsInstance(callbacks, list)
+            assert len(callbacks) == 0
